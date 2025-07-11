bdaa52857170afc10648eeb198d57f92b6b9279d
coder-xya <coderyuqian@gmail.com>
2025-07-11 14:18:29 +0800

    路由自动注册

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+
+
+/**
+ * 路由自动注册
+ */
+
+const fs = require('fs')
+
+function registerRouters (app){
+
+    const files = fs.readdirSync(__dirname)
+
+    console.log(files);
+
+    for(const file of files){
+        
+        if(!file.endsWith('.router.js')) continue
+
+        const router = require(`./${file}`)
+
+        app.use(router.routes())
+        app.use(router.allowedMethods())
+    }
+
+
+    
+}
+
+module.exports = {registerRouters}
